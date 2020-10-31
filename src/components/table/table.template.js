bf9034686e0e1390b8cfe829d7e714156f6b9f11
vol4ik europe <volikeurope@gmail.com>
2020-10-31 21:13:10 +0400

    Finish base table logic

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -1,12 +1,20 @@
 const CODES = {
   A: 65,
-  Z: 90
-}
+  Z: 90,
+};
 
-function toCell(_, col) {
-  return `
-    <div class="cell" contenteditable data-col="${col}"></div>
-  `
+function toCell(row) {
+  return function (_, col) {
+    return `
+      <div 
+        class="cell" 
+        contenteditable 
+        data-col="${col}"
+        data-type="cell"
+        data-id="${row}:${col}"
+      ></div>
+    `;
+  };
 }
 
 function toColumn(col, index) {
@@ -15,46 +23,45 @@ function toColumn(col, index) {
       ${col}
       <div class="col-resize" data-resize="col"></div>
     </div>
-  `
+  `;
 }
 
 function createRow(index, content) {
-  const resize = index ? '<div class="row-resize" data-resize="row"></div>' : ''
+  const resize = index
+    ? '<div class="row-resize" data-resize="row"></div>'
+    : "";
   return `
     <div class="row" data-type="resizable">
       <div class="row-info">
-        ${index ? index : ''}
+        ${index ? index : ""}
         ${resize}
       </div>
       <div class="row-data">${content}</div>
     </div>
-  `
+  `;
 }
 
 function toChar(_, index) {
-  return String.fromCharCode(CODES.A + index)
+  return String.fromCharCode(CODES.A + index);
 }
 
 export function createTable(rowsCount = 15) {
-  const colsCount = CODES.Z - CODES.A + 1 // Compute cols count
-  const rows = []
+  const colsCount = CODES.Z - CODES.A + 1; // Compute cols count
+  const rows = [];
 
-  const cols = new Array(colsCount)
-      .fill('')
-      .map(toChar)
-      .map(toColumn)
-      .join('')
+  const cols = new Array(colsCount).fill("").map(toChar).map(toColumn).join("");
 
-  rows.push(createRow(null, cols))
+  rows.push(createRow(null, cols));
 
-  for (let i = 0; i < rowsCount; i++) {
+  for (let row = 0; row < rowsCount; row++) {
     const cells = new Array(colsCount)
-        .fill('')
-        .map(toCell)
-        .join('')
+      .fill("")
+      // .map((_, col) => toCell(row, col))
+      .map(toCell(row))
+      .join("");
 
-    rows.push(createRow(i + 1, cells))
+    rows.push(createRow(row + 1, cells));
   }
 
-  return rows.join('')
+  return rows.join("");
 }
